b0967c267c188f79ccbba08708bae1f4318989c6
Isaac Marcos <isaacmarcos@scdesign.com.br>
2024-01-22 17:38:50 -0300

    :tada: connection database

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -2,6 +2,6 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
 import fetch from "cross-fetch"
 
 export const client = new ApolloClient({
-  link: new HttpLink({uri: "http://localhost:4000", fetch}),
+  link: new HttpLink({uri: "https://aparbsdash-server.vercel.app/", fetch}),
   cache: new InMemoryCache(),
 })
